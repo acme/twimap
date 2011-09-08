b52290a83a95f07434d3970c998d95e1a5e8aea3
Leon Brocard <acme@astray.com>
2011-09-08 15:10:02 +0100

    move logic into library

--- lib/App/Twimap.pm
+++ lib/App/Twimap.pm
@@ -7,11 +7,14 @@ use Email::MIME;
 use Email::MIME::Creator;
 use Encode;
 use HTML::Entities;
+use List::Util qw(max);
 use LWP::UserAgent;
 use Web::oEmbed::Common;
 use URI::WithBase;
-has 'mail_imapclient' => ( is => 'ro', isa => 'Mail::IMAPClient' );
-has 'net_twitter'     => ( is => 'rw', isa => 'Net::Twitter' );
+has 'mail_imapclient' =>
+    ( is => 'ro', isa => 'Mail::IMAPClient', required => 1 );
+has 'net_twitter' => ( is => 'ro', isa => 'Net::Twitter', required => 1 );
+has 'mailbox'     => ( is => 'ro', isa => 'Str',          required => 1 );
 
 my $parser = DateTime::Format::Strptime->new(
     pattern  => '%a %b %d %T %z %Y',
@@ -73,6 +76,7 @@ sub tweet_to_email {
             $text_offset += length($href) - length( $entity->{url} );
 
             my $consumer = Web::oEmbed::Common->new();
+
             #$consumer->set_embedly_api_key('0123ABCD0123ABCD0123ABCD');
             my $response = $consumer->embed($expanded_url);
             $html = encode_utf8( $response->render ) if $response;
@@ -109,6 +113,139 @@ sub tweet_to_email {
     );
 }
 
+sub imap_tids {
+    my $self    = shift;
+    my $imap    = $self->mail_imapclient;
+    my $mailbox = $self->mailbox;
+
+    warn "Fetching message_ids...";
+
+    $self->select_mailbox;
+
+    my $message_ids
+        = $imap->fetch_hash('BODY.PEEK[HEADER.FIELDS (Message-Id)]')
+        or die "Fetch hash $mailbox error: ", $imap->LastError;
+
+    my %tids;
+
+    foreach my $uid ( keys %$message_ids ) {
+        my $message_id
+            = $message_ids->{$uid}->{'BODY[HEADER.FIELDS (MESSAGE-ID)]'};
+        my ($tid) = $message_id =~ /Message-Id: <(\d+)\@twitter>/;
+        next unless $tid;
+        $tids{$tid} = 1;
+    }
+    return \%tids;
+}
+
+sub sync_home_timeline {
+    my $self    = shift;
+    my $twitter = $self->net_twitter;
+    my $tids    = $self->imap_tids;
+
+    my $since_id = max( keys %$tids );
+    my $max_id   = 0;
+    while (1) {
+        warn
+            "Fetching home timeline since id $since_id and max_id $max_id...";
+        my $tweets;
+        my $new_tweets = 0;
+        while (1) {
+            my $conf = {
+                count            => 100,
+                since_id         => $since_id,
+                include_entities => 1
+            };
+            $conf->{max_id} = $max_id if $max_id;
+            eval {
+                $tweets = $twitter->home_timeline($conf);
+                warn Dumper( $twitter->get_error ) unless $tweets;
+            };
+            last unless $@;
+            warn $@;
+            sleep 10;
+        }
+
+        foreach my $tweet (@$tweets) {
+
+            #        warn encode_json($tweet);
+            #use Data::Dumper; warn Dumper $tweet;
+            #exit;
+            my $tid = $tweet->{id};
+
+            $max_id = $tid unless $max_id;
+            $max_id = $tid if $tid < $max_id;
+
+            next if $tids->{$tid};
+            $new_tweets++;
+
+            #warn $tid . ' ' . $tweet->{text};
+
+            #use YAML;
+            #warn Dump $tweet;
+
+            my $email = $self->tweet_to_email($tweet);
+
+            warn $email->as_string;
+            $self->append_email($email);
+            $tids->{$tid} = 1;
+        }
+        last unless $new_tweets;
+        sleep 30;
+    }
+}
+
+sub sync_replies {
+    my $self    = shift;
+    my $imap    = $self->mail_imapclient;
+    my $mailbox = $self->mailbox;
+    my $tids    = $self->imap_tids;
+
+    warn "Fetching in_reply_tos...";
+
+    $self->select_mailbox;
+
+    my $replies = $imap->fetch_hash('BODY.PEEK[HEADER.FIELDS (IN-REPLY-TO)]')
+        or die "Fetch hash $mailbox error: ", $imap->LastError;
+    foreach my $uid ( keys %$replies ) {
+        my $header = $replies->{$uid}->{'BODY[HEADER.FIELDS (IN-REPLY-TO)]'};
+        my ($tid) = $header =~ /In-Reply-To: <(\d+)\@twitter>/;
+        next unless $tid;
+        next if $tids->{$tid};
+        $self->fetch_tid($tid);
+        sleep 30;
+    }
+}
+
+sub fetch_tid {
+    my ( $self, $tid ) = @_;
+    my $twitter = $self->net_twitter;
+
+    warn "Fetching tweet $tid...";
+
+    my $tweet = $twitter->show_status( $tid, { include_entities => 1 } );
+
+    #       warn encode_json($tweet);
+    #use Data::Dumper; warn Dumper $tweet;
+
+    warn $tweet->{created_at} . ' ' . $tweet->{text};
+
+    my $email = $self->tweet_to_email($tweet);
+
+    #warn $email->as_string;
+    $self->append_email($email);
+
+    #say $uidort;
+}
+
+sub append_email {
+    my ( $self, $email ) = @_;
+    my $imap    = $self->mail_imapclient;
+    my $mailbox = $self->mailbox;
+    my $uid     = $imap->append_string( $mailbox, $email->as_string )
+        or die "Could not append_string to $mailbox: ", $imap->LastError;
+}
+
 sub expand_url {
     my ( $self, $url ) = @_;
     my $ua = LWP::UserAgent->new(
@@ -126,6 +263,14 @@ sub expand_url {
         return $self->expand_url($uri);
     }
     return $self->expand_url($location);
+}
+
+sub select_mailbox {
+    my $self    = shift;
+    my $imap    = $self->mail_imapclient;
+    my $mailbox = $self->mailbox;
+    $imap->select($mailbox)
+        or die "Select $mailbox error: ", $imap->LastError;
 }
 
 1;
